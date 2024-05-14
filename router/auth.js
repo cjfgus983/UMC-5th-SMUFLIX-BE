b578286b4f879c3ed35d621f97bd25d00db96703
김용민 <dydals3440@gmail.com>
2024-05-14 13:01:22 +0900

    fix: 워크북 미션 에러 수정

--- router/auth.js
+++ router/auth.js
@@ -8,34 +8,34 @@ import { isAuth } from '../middleware/auth.js';
 const router = express.Router();
 
 const validateCredential = [
-  body('username')
-    .trim()
-    .isLength({ min: 5 })
-    .notEmpty()
-    .withMessage('아이디는 최소 5자리 이상으로 구성해주세요!'),
-  body('password')
-    .trim()
-    .isLength({ min: 4, max: 12 })
-    .notEmpty()
-    .withMessage(
-      '비밀번호는 대소문자, 특수문자 포함 최소 4자리 이상 12자리 이하로 구성해주세요!'
-    )
-    .matches(
-      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[a-zA-Z0-9!@#$%^&*()]{8,16}$/
-    ),
-  validate,
+	body('username')
+		.trim()
+		.isLength({ min: 5 })
+		.notEmpty()
+		.withMessage('아이디는 최소 5자리 이상으로 구성해주세요!'),
+	body('password')
+		.trim()
+		.isLength({ min: 4, max: 12 })
+		.notEmpty()
+		.withMessage(
+			'비밀번호는 대소문자, 특수문자 포함 최소 4자리 이상 12자리 이하로 구성해주세요!'
+		)
+		.matches(
+			/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[a-zA-Z0-9!@#$%^&*()]{4,12}$/
+		),
+	validate,
 ];
 
 const validateSignup = [
-  ...validateCredential,
-  body('name').notEmpty().withMessage('이름을 작성해주세요!'),
-  body('email')
-    .isEmail()
-    .notEmpty()
-    .normalizeEmail()
-    .withMessage('잘못된 형식의 이메일입니다.'),
-  body('age').isNumeric().notEmpty(),
-  validate,
+	...validateCredential,
+	body('name').notEmpty().withMessage('이름을 작성해주세요!'),
+	body('email')
+		.isEmail()
+		.notEmpty()
+		.normalizeEmail()
+		.withMessage('잘못된 형식의 이메일입니다.'),
+	body('age').isNumeric().notEmpty(),
+	validate,
 ];
 router.post('/signup', validateSignup, authController.signup);
 
